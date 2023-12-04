715d63f1debf26215afe8b21c4a2a3d384ce7294
naterattner <naterattner@gmail.com>
2023-12-03 21:25:19 -0500

    add data, start building

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
-newdiv = document.createElement('div');
-document.querySelector('body').appendChild(newdiv);
-console.log('HI IM WORKING');
+// Function to fetch a random subset of items from the JSON data
+function getRandomItems(data, count) {
+	const shuffled = data.sort(() => 0.5 - Math.random());
+	return shuffled.slice(0, count);
+  }
 
-const obj = { name: 'omar' };
+ function loadData(path, numItems) {
+	fetch(path)
+		.then(response => response.json())
+		.then(data => {
+			const randomItems = getRandomItems(data, numItems); // Get a random selection of numItems
+			
+			// Process and display the randomly selected items
+			randomItems.forEach(item => {
+			// Create elements for each item and display them on the page
+			const element = document.createElement('div');
+			element.textContent = item.name; // Replace 'name' with your JSON property
+			document.body.appendChild(element); // Append to the document body or your desired container
+			});
+		})
+		.catch(error => console.log('Error fetching data:', error));
+
+ } 
+ 
+ 
+ loadData('./assets/data/merged_data.json')
