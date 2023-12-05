58c1519498a84d48c70762174a6ef5c037ebe6e7
naterattner <naterattner@gmail.com>
2023-12-04 21:42:52 -0500

    data is loading

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,31 @@
-// Function to fetch a random subset of items from the JSON data
-function getRandomItems(data, count) {
+// Function to fetch the JSON file
+async function fetchJSONFile() {
+	try {
+	  const response = await fetch('./assets/data/merged_data_2021.json');
+	  const data = await response.json();
+	  return data;
+	} catch (error) {
+	  console.error('Error fetching data:', error);
+	  return null;
+	}
+  }
+  
+  // Function to get a random subset of items
+  function getRandomItems(data, count) {
 	const shuffled = data.sort(() => 0.5 - Math.random());
 	return shuffled.slice(0, count);
   }
-
- function loadData(path, numItems) {
-	fetch(path)
-		.then(response => response.json())
-		.then(data => {
-			const randomItems = getRandomItems(data, numItems); // Get a random selection of numItems
-			
-			// Process and display the randomly selected items
-			randomItems.forEach(item => {
-			// Create elements for each item and display them on the page
-			const element = document.createElement('div');
-			element.textContent = item.name; // Replace 'name' with your JSON property
-			document.body.appendChild(element); // Append to the document body or your desired container
-			});
-		})
-		.catch(error => console.log('Error fetching data:', error));
-
- } 
- 
- 
- loadData('./assets/data/merged_data.json')
+  
+  // Fetch and process the data
+  async function processData() {
+	const jsonData = await fetchJSONFile();
+	if (jsonData) {
+	  const randomItems = getRandomItems(jsonData, 100); // Get a random selection of 100 items
+	  console.log(randomItems); // Do something with the randomly selected items
+	} else {
+	  console.error('Failed to fetch data.');
+	}
+  }
+  
+  // Call the function to initiate data processing
+  processData();
